a029455c773cb8b68293eeff02959b0157254c75
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-10-02 16:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+  // Get the dropdown element
+  const colorSelect = document.getElementById("colorSelect");
+
+  // Remove the selected option from the dropdown
+  colorSelect.remove(colorSelect.selectedIndex);
+}
